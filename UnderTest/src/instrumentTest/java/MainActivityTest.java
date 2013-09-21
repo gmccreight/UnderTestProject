d9575191c56baf51778cc5140c6be1ca3cb4c23e
Gordon McCreight <gordon@mccreight.com>
2013-09-20 22:16:07 -0700

    Add a single test that passes

---
+++ UnderTest/src/instrumentTest/java/MainActivityTest.java
@@ -0,0 +1,9 @@
+import android.test.ActivityTestCase;
+
+public class MainActivityTest extends ActivityTestCase {
+
+    public void testThatPasses() {
+        assertEquals("it works", "foo", "foo");
+    }
+
+}
